95d6dcb9e01d92a4661feaebcf8b37a363d968ae
NoSequel <nosequel@protonmail.com>
2021-08-01 18:30:44 +0200

    change QueuePlatform to abstract class

--- bukkit/src/main/java/io/github/nosequel/queue/bukkit/BukkitQueuePlatform.java
+++ bukkit/src/main/java/io/github/nosequel/queue/bukkit/BukkitQueuePlatform.java
@@ -5,24 +5,13 @@ import io.github.nosequel.queue.shared.model.player.QueuePlayerHandler;
 import io.github.nosequel.queue.shared.model.queue.QueueHandler;
 import io.github.nosequel.queue.shared.model.server.ServerHandler;
 
-public class BukkitQueuePlatform implements QueuePlatform {
+public class BukkitQueuePlatform extends QueuePlatform {
 
-    private final QueueHandler queueHandler = new QueueHandler();
-    private final QueuePlayerHandler queuePlayerHandler = new QueuePlayerHandler();
-    private final ServerHandler serverHandler = new ServerHandler();
-
-    @Override
-    public QueueHandler getQueueHandler() {
-        return this.queueHandler;
-    }
-
-    @Override
-    public QueuePlayerHandler getQueuePlayerHandler() {
-        return this.queuePlayerHandler;
-    }
-
-    @Override
-    public ServerHandler getServerHandler() {
-        return this.serverHandler;
+    public BukkitQueuePlatform() {
+        super(
+                new QueueHandler(),
+                new QueuePlayerHandler(),
+                new ServerHandler()
+        );
     }
 }
--- shared/src/main/java/io/github/nosequel/queue/shared/QueuePlatform.java
+++ shared/src/main/java/io/github/nosequel/queue/shared/QueuePlatform.java
@@ -3,13 +3,15 @@ package io.github.nosequel.queue.shared;
 import io.github.nosequel.queue.shared.model.player.QueuePlayerHandler;
 import io.github.nosequel.queue.shared.model.queue.QueueHandler;
 import io.github.nosequel.queue.shared.model.server.ServerHandler;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
-public interface QueuePlatform {
+@Getter
+@RequiredArgsConstructor
+public abstract class QueuePlatform {
 
-    QueueHandler getQueueHandler();
-
-    QueuePlayerHandler getQueuePlayerHandler();
-
-    ServerHandler getServerHandler();
+    private final QueueHandler queueHandler;
+    private final QueuePlayerHandler queuePlayerHandler;
+    private final ServerHandler serverHandler;
 
 }
